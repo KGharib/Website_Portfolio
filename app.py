3e2ebfea0cf100bb2f0325241445d8fa848ebfcb
KGharib <Khalid.Gharib1994@gmail.com>
2020-10-04 23:19:06 +0100

    debugged

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import os
 # Configure application
 app = Flask(__name__)
 app.static_folder = 'static'
-app.template_folder='templates'
-subscribers = []
+# app.template_folder='templates'
+# subscribers = []  
 
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/portfolio')
 def portfolio():
@@ -38,25 +39,25 @@ def subscribe():
     title = 'Subscribe - Khalid Gharib'
     return render_template('subscribe.html')
 
-@app.route('/form', methods=['POST'])
-def form():
-    title = 'Form'
-    first_name= request.form.get("first_name")
-    last_name= request.form.get("last_name")
-    email = request.form.get("email")
-    # message = 'You have subscribed to my Email newsletter'
+# @app.route('/form', methods=['POST'])
+# def form():
+#     title = 'Form'
+#     first_name= request.form.get("first_name")
+#     last_name= request.form.get("last_name")
+#     email = request.form.get("email")
+#     # message = 'You have subscribed to my Email newsletter'
 
-    # server = smtplib.SMTP("smtp.gmail.com", 587)
-    # server.starttls()
-    # server.login("khalid.gharib1994@gmail.com", os.getenv("PASSWORD"))
-    # server.sendmail("khalid.gharib1994@gmail.com", email, message)
+#     # server = smtplib.SMTP("smtp.gmail.com", 587)
+#     # server.starttls()
+#     # server.login("khalid.gharib1994@gmail.com", os.getenv("PASSWORD"))
+#     # server.sendmail("khalid.gharib1994@gmail.com", email, message)
 
-    if not first_name or not last_name or not email:
-        error_statement = 'Missing Fields, please try again.'
-        return render_template('subscribe.html', error_statement=error_statement
-                                ,first_name=first_name
-                                ,last_name=last_name
-                                ,email=email)
-    subscribers.append(f'{first_name} {last_name} | {email}')
-    return render_template('form.html',subscribers=subscribers)
+#     if not first_name or not last_name or not email:
+#         error_statement = 'Missing Fields, please try again.'
+#         return render_template('subscribe.html', error_statement=error_statement
+#                                 ,first_name=first_name
+#                                 ,last_name=last_name
+#                                 ,email=email)
+#     subscribers.append(f'{first_name} {last_name} | {email}')
+#     return render_template('form.html',subscribers=subscribers)
 
